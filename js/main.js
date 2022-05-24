24c189b7254d8f02445cde61da22b5c2c836db8c
Cassie Lee <95990259+casslee8757@users.noreply.github.com>
2022-05-24 14:24:44 +1000

    field refactoring

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
-const CARROT_SIZE = 80;
+import PopUp from './popup.js';
+import Field from './field.js';
+
 const CARROT_COUNT = 20;
 const BUG_COUNT = 20;
 const GAME_DURATION_SEC = 20;
 
-const field = document.querySelector('.game__field')
-const fieldRect = field.getBoundingClientRect();
+
 const gameBtn = document.querySelector('.game__button')
 const gameTimer = document.querySelector('.game__timer')
 const gameScore = document.querySelector('.game__score')
-const popUp = document.querySelector('.pop-up')
-const popUpText = document.querySelector('.pop-up__message')
-const popUpRefresh = document.querySelector('.pop-up__refresh')
+
 
 const carrotSound = new Audio('./sound/carrot_pull.mp3')
 const alertSound = new Audio('./sound/alert.wav')
@@ -22,7 +21,31 @@ let started = false;
 let score = 0;
 let timer = undefined;
 
-field.addEventListener('click', (ev) => onFieldClick(ev))
+const gameFinishBanner = new PopUp()
+gameFinishBanner.setClickListener( () => {
+    startGame()
+} )
+
+const gameField = new Field(CARROT_COUNT, BUG_COUNT)
+gameField.setClickListener(onItemClick)
+
+
+function onItemClick(item){
+    if(!started) {
+        return;
+    }
+
+    if(item === 'carrot'){
+        score++;
+        updateScoreBoard();
+        if(score === CARROT_COUNT){
+            finishGame(true)
+        }
+    }else if(item === 'bug'){
+        finishGame(false)
+            
+    }
+}
 
 gameBtn.addEventListener('click', () => {
     if(started){
@@ -33,10 +56,6 @@ gameBtn.addEventListener('click', () => {
     // started = !started;
 });
 
-popUpRefresh.addEventListener('click', () => {
-    startGame()
-    hidePopUp()
-})
 
 const startGame = () => {
     started = true;
@@ -51,7 +70,7 @@ const stopGame = () => {
     started = false;
     stopGameTimer()
     hideStartButton()
-    showPopUpWithText('REPLAY?')
+    gameFinishBanner.showWithText('REPLAY?')
     playSound(alertSound)
     stopSound(bgSound)
 }
@@ -66,7 +85,7 @@ const finishGame = win => {
     }
     stopGameTimer(); 
     stopSound(bgSound)
-    showPopUpWithText(win? 'YOU WON!' : 'YOU LOST')
+    gameFinishBanner.showWithText(win? 'YOU WON!' : 'YOU LOST')
 }
 
 const showStopButton = () => {
@@ -109,41 +128,19 @@ const stopGameTimer = () => {
     clearInterval(timer)
 }
 
-const showPopUpWithText = (text) => {
-    popUpText.innerHTML = text;
-    popUp.classList.remove('pop-up--hide')
-   
-}
-
-const hidePopUp = () => {
-    popUp.classList.add('pop-up--hide')
-}
 
 const initGame = () => {
     score = 0;
-    field.innerHTML = '';
-    gameScore.innerText = CARROT_COUNT
-    addItem('carrot', CARROT_COUNT, 'img/carrot.png')
-    addItem('bug', BUG_COUNT, 'img/bug.png')
+    gameScore.innerText = CARROT_COUNT;
+    gameField.init();
+    
 }
 
 const onFieldClick = event => {
     if(!started){
         return
     }
-    const target = event.target;
-    if(target.matches('.carrot')){
-        target.remove()
-        score++
-        playSound(carrotSound)
-        updateScoreBoard()
-        if(score === CARROT_COUNT){
-
-            finishGame(true);
-        }
-    }else if(target.matches('.bug')){
-        finishGame(false)
-    }
+    
 }
 
 const playSound = sound => {
@@ -159,25 +156,4 @@ const updateScoreBoard = () => {
     gameScore.innerText = CARROT_COUNT - score;
 }
 
-const addItem = (className, count, imgPath) => {
-    const x1 = 0;
-    const y1 = 0;
-    const x2 = fieldRect.width - CARROT_SIZE;
-    const y2 = fieldRect.height - CARROT_SIZE;
-    for (let i = 0; i < count; i++) {
-        const item = document.createElement('img');
-        item.setAttribute('class', className);
-        item.setAttribute('src', imgPath);
-        item.style.position = 'absolute';
-        const x = randomNumber(x1, x2);
-        const y = randomNumber(y1, y2);
-        item.style.left = `${x}px`
-        item.style.top = `${y}px`
-        field.appendChild(item)
-        
-    }
-}
 
-const randomNumber = (min, max) => {
-    return Math.random() * (max - min) + min;
-}
