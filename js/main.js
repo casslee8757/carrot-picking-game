ce253687e37768b1677488ed4b76bdd6d6c1f35c
Cassie Lee <95990259+casslee8757@users.noreply.github.com>
2022-06-06 17:21:37 +1000

    CSS refactoring

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ import { GameBuilder, Reason } from './game.js'
 
 const gameFinishBanner = new PopUp()
 const game = new GameBuilder()
-.withGameDuration(5)
-.withCarrotCount(3)
-.withBugCount(3)
+.withGameDuration(20)
+.withCarrotCount(20)
+.withBugCount(20)
 .build()
 
 game.setGameStopListener(reason => {
